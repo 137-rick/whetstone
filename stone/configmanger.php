194ceb4d829b3d515d8410f91aa8cf38d8ffc4b7
徐长龙 <xcl_rockman@qq.com>
2019-01-17 17:12:43 +0800

    load config
    load config

--- stone/configmanger.php
+++ stone/configmanger.php
@@ -9,4 +9,21 @@ namespace WhetStone\Stone;
  */
 class ConfigManager{
 
+    private static $config = array();
+
+    public function loadConfig($name){
+        $configPath = dirname(dirname(__DIR__))."/config/";
+        if(file_exists($configPath.$name.".php")){
+            self::$config[$name] = include $configPath.$name.".php";
+        }
+        throw new \Exception("load Config was not found",-118);
+    }
+
+    public function getConfig($name){
+        if(isset(self::$config[$name])){
+            return self::$config[$name];
+        }
+
+        throw new \Exception("Config was not found",-119);
+    }
 }
