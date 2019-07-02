77dda37d71bd6059abd04d6f465e296c82db434f
xuchanglong <xuchanglong@100tal.com>
2019-07-02 17:32:51 +0800

    add respone format by miao
    add respone format by miao

--- controller/test/test.php
+++ controller/test/test.php
@@ -1,14 +1,13 @@
 <?php
-namespace WhetStone\Controller\Test;
 
-use WhetStone\Stone\Context;
+namespace WhetStone\Controller\Test;
 
 class Test extends \WhetStone\Stone\Controller
 {
 
     public function index($context, $request, $response)
     {
-        return $this->showJson("success",0);
+        return $this->response("success", 0);
     }
 
     public function info($context, $request, $response)
@@ -18,18 +17,18 @@ class Test extends \WhetStone\Stone\Controller
         $redis = \WhetStone\Stone\Driver\Redis\Redis::Factory("test_redis_shard");
 
         mt_srand(time());
-        $redis->set("a".mt_rand(1,1000000),"aa");
+        $redis->set("a" . mt_rand(1, 1000000), "aa");
         /*$ret = $redis->get("a");
         if($ret != "aa"){
             echo 12;
         }*/
-        return $this->showJson("nothing to do",-123,$param);
+        return $this->response("nothing to do", -123, $param);
     }
 
     public function status($context, $request, $response)
     {
-        $redis = \WhetStone\Stone\Driver\Redis\Redis::Factory("test_redis_shard");
+        $redis      = \WhetStone\Stone\Driver\Redis\Redis::Factory("test_redis_shard");
         $connection = $redis->getConnectionStatus();
-        return $this->showJson("OK",0,$connection);
+        return $this->response("OK", 0, $connection);
     }
 }
--- stone/controller.php
+++ stone/controller.php
@@ -7,9 +7,18 @@ class Controller
     public function showJson($msg, $code, $data = array())
     {
         return json_encode(array(
-                "code" => $code,
-                "msg"  => $msg,
-                "data" => $data,
-            ),JSON_BIGINT_AS_STRING);
+            "code" => $code,
+            "msg"  => $msg,
+            "data" => $data,
+        ), JSON_BIGINT_AS_STRING);
+    }
+
+    public function response($msg, $code, $data = array(), $format = "json")
+    {
+        switch ($format) {
+            case "json":
+            default:
+                return $this->showJson($msg, $code, $data);
+        }
     }
 }
